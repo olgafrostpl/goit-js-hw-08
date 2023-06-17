859b865112e90f81a203b2a029fd815139b1bf34
olgafrostpl <123272546+olgafrostpl@users.noreply.github.com>
2023-06-18 00:57:10 +0300

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import '../css/common.css';
+import '../css/01-gallery.css';
+
+const galleryItemsMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+  })
+  .join('');
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.innerHTML = galleryItemsMarkup;
+// galleryEl.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+let lightbox = new SimpleLightbox('.gallery a', {
+  scrollZoom: false,
+  captionDelay: 250,
+  captionsData: 'alt',
+  doubleTapZoom: 1,
+});
+galleryEl.addEventListener('click', event => {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+});
 
 console.log(galleryItems);
