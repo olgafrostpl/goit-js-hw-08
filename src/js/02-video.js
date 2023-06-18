65bef93842c3376c964673bfccecc0b6e3be1454
olgafrostpl <123272546+olgafrostpl@users.noreply.github.com>
2023-06-18 14:02:27 +0300

    player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,31 @@
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-player.on('timeupdate', throttle(onPlay, 1000));
-
-function onPlay({ seconds }) {
+const onTimeupdate = function ({ seconds }) {
   localStorage.setItem('videoplayer-current-time', seconds);
-}
+};
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.on('timeupdate', throttle(onTimeupdate, 1000));
+const timeStart = localStorage.getItem('videoplayer-current-time');
+
+if (timeStart) {
+  player
+    .setCurrentTime(timeStart)
+    .then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+}
